f5efc5c5ccdfeeb87566938a039a1a3b49a5e06f
Elisa Andrade <elisalvsan@gmail.com>
2024-07-21 11:59:06 -0300

    :sparkles: feat:Criando o Tamplate base

---
+++ src/templates/user-template/index.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+import Footer from "../../components/footer";
+import Header from "../../components/header";
+
+type UserTemplateProps = PropsWithChildren;
+
+export default function UserTemplate(props: UserTemplateProps) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1 p-2 m-2 font-inter">{props.children}</div>
+      <Footer />
+    </div>
+  );
+}
